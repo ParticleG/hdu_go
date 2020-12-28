529e5f94777b47226b2f5df8f3ae668689ed1a08
Particle_G <1135989508@qq.com>
2020-12-28 19:35:11 +0800

    Add Quasar plugin

---
+++ src/quasar.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+
+import './styles/quasar.scss'
+import lang from 'quasar/lang/zh-hans.js'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import '@quasar/extras/eva-icons/eva-icons.css'
+import { Quasar } from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  plugins: {
+  },
+  lang: lang
+ })
